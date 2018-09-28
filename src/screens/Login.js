06fe74896d9b63774682efad73ea7f7fa1e10030
jemca7 <jjcm238@gmail.com>
2018-09-28 13:25:10 -0600

    cambios login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import Fondo from './components/fondo';
+import {
+  StyleSheet,
+  View, 
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  AsyncStorage,
+} from 'react-native';
+//import Fondo from '../components/fondo';
 
 class Login extends Component {
+constructor(props){
+  super(proprs);
+  this.state{
+    username:'';,
+    password:'',
+  }
+}
+componentDiMount(){
+ this._loadInitialState().done();
+}
+_loadInitialState =async () => {
+  var value = await AsyncStorage.getItem('user');
+  if (value !== null){
+    this.props.navigation.navigate('Profile');
+  }
+}
+
   render() {
     return (
-        <Fondo>
-        </Fondo>
+        <KeyboardAvoidingView behavior='padding' style={styles.wrapper}>
+             <View style={styles.container}>
+             <Text style={style.header}>LOGIN </Text>
+             <TextInput
+                   style={styles.}
+             </View>
+        </KeyboardAvoidingView>
     );
   }
 }
 
-export default Fondo;
+export default Login;
